b04117952152986c9cfe6103c20bba352ef77260
Ysaito-prot <yu22.program@gmail.com>
2023-11-07 11:30:03 +0900

    トップページ日程選択以外の機能実装完了

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+
+const mysql = require("mysql2");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "flight_ticket_reservation_db",
+  multipleStatements: true,
+});
+
+// cssファイルの取得
+app.use(express.static("assets"));
+
+// mysqlからデータを持ってくる
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "html/index.html"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
